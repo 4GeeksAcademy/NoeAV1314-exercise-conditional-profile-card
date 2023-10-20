d88badd058869df20f2e643e7a6f576f5e758a78
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2023-10-20 20:30:50 +0000

    Estoycansadojefe

--- src/js/app.js
+++ src/js/app.js
@@ -36,11 +36,11 @@ function render(variables = {}) {
           <h1>"${variables.name} ${variables.lastname} "</h1>
           <h2>"${variables.role}"</h2>
           <h3>"${variables.city} ${variables.country}"</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fa-brands fa-twitter fa-2xs"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fa-brands fa-github fa-2xs"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fa-brands fa-linkedin fa-2xs"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fa-brands fa-instagram fa-2xs"></i></a></li>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa-brands fa-twitter fa-2xs"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa-brands fa-github fa-2xs"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa-brands fa-linkedin fa-2xs"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa-brands fa-instagram fa-2xs"></i></a></li>
           </ul>
         </div>
     `;
